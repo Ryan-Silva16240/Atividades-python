1e8b15f11e59a8479fd22511a9ab026446f8f779
Ryan-Silva16240 <ryan.silva23@etepd.com>
2025-03-25 10:23:04 -0300

    Create História interativa.py

---
+++ História interativa.py
@@ -0,0 +1,9 @@
+nome = input("Qual é o nome do seu personagem? ")
+idade = int(input("Qual é a idade do seu personagem? "))
+poder = input("Qual o poder do seu personagem? ")
+amigos = input("Quais são os amigos do seu personagem? ")
+cenario = input("Qual é o cenário em que os personagens estão inseridos? ")
+vilao = input("Qual é o vilão que seu personagem está enfrentando? ")
+poderDoVilao = input("Qual é o poder do vilão? ")
+
+print(f"O bravo guerreiro {nome}, ao auge dos seus {idade} anos, possuía o poder de {poder}, e ao lado dos seus amigos {amigos}, no/na {cenario}, enfrentou o poderoso vilão {vilao}, que possuía o poder de {poderDoVilao}, e o venceram bravamente.")
